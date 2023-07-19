31865bea343f60802bfe685fff7e311897797335
Konstantin Mishakov <mishakov1c@gmail.com>
2023-07-20 00:22:52 +0400

    added first test

--- webapp/tests/test_main.py
+++ webapp/tests/test_main.py
@@ -1,12 +1,25 @@
-# from unittest.mock import patch
-# # from webapp.main import get_bored_activity, get_unsplash_picture, get_advice
+from unittest.mock import patch
+from fastapi.testclient import TestClient
+from webapp import main
 
 
-# def test__get_advice__success():
-#     with (
-#         patch('main.get_bored_activity') as get_bored_activity_mock,
-#         patch('main.get_unsplash_picture') as get_unsplash_picture_mock,
-#     ):
+def test__get_advice__success():
+    with (
+        patch('webapp.main.get_bored_activity') as get_bored_activity_mock,
+        patch('webapp.main.get_unsplash_picture') as get_unsplash_picture_mock,
+    ):
 
-#         get_bored_activity_mock.return_value = "Research a topic you're interested in"
-#         get_unsplash_picture_mock.return_value = "https://images.unsplash.com/photo-1532187863486-abf9dbad1b69?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzM2MDJ8MHwxfHNlYXJjaHwxfHxSZXNlYXJjaCUyMGElMjB0b3BpYyUyMHlvdSUyN3JlJTIwaW50ZXJlc3RlZCUyMGlufGVufDB8fHx8MTY4OTM2MDUyMHww&ixlib=rb-4.0.3&q=80&w=400"
+        get_bored_activity_mock.return_value = "Research a topic you're interested in"
+        get_unsplash_picture_mock.return_value = "https://images.unsplash.com/photo-153218786.jpg"
+
+        client = TestClient(main.app)
+        response = client.get("/")
+        print('start')
+        print(response.template)
+        print('finish')
+        assert response.status_code == 200
+        assert response.template.name == 'index.html'
+        assert response.context['activity'] == "Research a topic you're interested in"
+        assert response.context['picture_link'] == "https://images.unsplash.com/photo-153218786.jpg"
+        get_bored_activity_mock.assert_called_once()
+        get_unsplash_picture_mock.assert_called_once_with("Research a topic you're interested in")
